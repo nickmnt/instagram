c83989c610f58174d9f72466f028977743f41928
nimamt <mtaghavinima@gmail.com>
2023-09-13 18:25:45 -0400

    Fix Github pages

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ export default function App() {
             <ContentContext.Provider value={{ setPopupContent }}>
                 <UserContext.Provider value={{ user }}>
                     <TermProvider>
-                        <Router basename="/instagram/">
+                        <Router>
                             <Suspense fallback={<ReactLoader />}>
                                 <Switch>
                                     <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
