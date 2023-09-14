7f2ddd80b591b8dc984337a3a13c7ba953dab9b2
Nima <mtaghavinima@gmail.com>
2023-09-14 07:18:30 -0400

    Added UnoCSS (with Tailwind classes)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS()],
   base: '/instagram/',
 })
